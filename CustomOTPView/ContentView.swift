2049b92d67cfaccaec3eb2c2afe3ae035eb0da52
HsynLoyiii <hsynloyiii@gmail.com>
2022-02-08 12:01:56 +0330

    Initial Commit

---
+++ CustomOTPView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomOTPView
+//
+//  Created by mohamad hosein hakimi on 2/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomOTPView/CustomOTPViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomOTPViewApp.swift
+//  CustomOTPView
+//
+//  Created by mohamad hosein hakimi on 2/8/22.
+//
+
+import SwiftUI
+
+@main
+struct CustomOTPViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
